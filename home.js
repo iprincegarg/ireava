23fbbec6e52495cbeb89d3c509caaa45a9f678bc
iprincegarg <82464363+iprincegarg@users.noreply.github.com>
2024-01-06 19:05:15 +0530

    Create home.js

---
+++ home.js
@@ -0,0 +1,341 @@
+var homeline1_1 = "100% All In One Complete ERP Solution For School",
+    homeline1_2 = "Help Your School To Go Digitally In The No Time",
+    homeline1_3 = "Ireava Creates Your School Digital Presence Effectively",
+    homeline1_4 = "Switch To New Interactive Teaching Way In No Time",
+    homeline2_1 = "Automate Modules. Quality Assured. 100% Complete Powerful ERP.",
+    homeline2_2 = "Manage Everything Using the Centralised SaaS Platform.",
+    homeline2_3 = "Posts on Social Media. Build Backlinking. Brand Awareness.",
+    homeline2_4 = "Teach With the Interative Hologram and Projector Content.",
+    homeline3 = "Serving to many schools globally. Recognised by Startup India and ISB.",
+    homeline4 = "Modules of",
+    homeline5 = "Ireava School ERP",
+    homeline6 = "Simple, powerful and dominant school management software",
+    homeline7 = "READ WHAT SOME OF OUR CUSTOMERS ARE SAYING",
+    homelist1 = [{
+        count: "115",
+        name: "Schools",
+        icon: "fas fa-school"
+    }, {
+        count: "140000",
+        name: "Students",
+        icon: "fas fa-user-graduate"
+    }, {
+        count: "12",
+        name: "Cities",
+        icon: "fas fa-building"
+    }, {
+        count: "93000",
+        name: "Online Exams",
+        icon: "fas fa-laptop"
+    }, {
+        count: "87000000",
+        name: "Online Fees",
+        icon: "fas fa-wallet"
+    }, {
+        count: "390 GB",
+        name: "Data Stored",
+        icon: "fas fa-database"
+    }],
+    homelist2 = [{
+        whatsnew: "displayn",
+        whatsnewtext: "What's new?",
+        line1: "Pricing starts only at",
+        boldline: "₹ 1,999/month ",
+        line2: "for 100% complete ERP, automate modules and mobile application.",
+        btn1link: "tel:+91-7015097673",
+        btn1text: "Call Us",
+        btn2link: "pricing",
+        btn2text: "Pricing"
+    }],
+    homelist3 = [{
+        img: "4267567",
+        imgalt: "Digitalise school with ireava",
+        title: "Digitalise Your School - Ireava ERP",
+        text: "We help you to go digital in no time. Get everything done on clicks. Monitor students' growth, get analyzed reports of individual students. We also help you to manage your school activities, infrastructure, transport, finance, academics, inventory, online classes and exams, report cards, and 30+ other deep researched modules.",
+        link: "school-erp"
+    }, {
+        img: "4787543",
+        imgalt: "Learning with ireava",
+        title: "New Way Learning - Ireava learning",
+        text: "Teaching on boards with the chalk and the marker was really an 80's method. Teaching with videos is also not better for learning. Today, we need something new. A new future is here with Ireava. We provide you an application using which you can teach Interactive and Engageable content via Projector and Hologram Classes.",
+        link: "elearning"
+    }, {
+        img: "5149654",
+        imgalt: "Branding with ireava",
+        title: "Brand Building - Online Marketing",
+        text: "Today's generation searches everything online. They check for the reviews and do brand verification. The same works for the schools. There results and branding matter. To target today's parents it's easy, just build your school presence online effectively. We help to get these things done so you can get more admissions every year.",
+        link: "brand-building"
+    }],
+    homelist4 = [{
+        icon: "advanced_web_tech",
+        iconalt: "Ireava Technology",
+        text1: "Technology",
+        text2: "Stack",
+        line: "Ireava is developed on the secure and most trusted language framework. Ireava software has a smooth interface, faster data transfer and a better optimized secure platform."
+    }, {
+        icon: "secure_risk",
+        iconalt: "Secure and risk free school software",
+        text1: "SECURE AND RISK",
+        text2: "FREE",
+        line: "Security is everyone's concern and it's our priority to take care of it. We ensure full security for data and providing totally secure and risk-free school management software."
+    }, {
+        icon: "flexible_pricing",
+        iconalt: "School software pricing",
+        text1: "Flexible",
+        text2: "Pricing",
+        line: "Ireava provides various enticing packs for school ERP services at giveaway prices. A user can also select a number of modules in a customized package that suits to work and pay as you go."
+    }, {
+        icon: "user_friendly_design",
+        iconalt: "User Friendly Design",
+        text1: "User Friendly",
+        text2: "Design",
+        line: "Ireava is the Craftsman of beautifying branded user experience. A user can access everything with one click with a simple and conductive design of school management software."
+    }, {
+        icon: "secure_payment_gateway",
+        iconalt: "Secure Payment Gateway",
+        text1: "Payment",
+        text2: "Method",
+        line: "Ireava is partnered with more than 5 payment gateway companies. We provide you the best and secure platform in the market for fast and secure payments collection at affordable transactional charges."
+    }, {
+        icon: "customer_support",
+        iconalt: "Customer Support",
+        text1: "Customer",
+        text2: "Support",
+        line: "Ireava is always available for customers, yes 24x7 available to provide personalized customer service and experience. Ireava delivers a superior customer experience across all customer touchpoints."
+    }, {
+        icon: "esay_backup",
+        iconalt: "school software backup",
+        text1: "Easy Backup",
+        text2: "Option",
+        line: "In order to provide more safety, a user will get an easy backup option in Ireava ERP to create a backup of data and import it anytime. We also provide a daily auto-backup option in standard and premium plans."
+    }, {
+        icon: "one_touch_customization",
+        iconalt: "school software customization",
+        text1: "Easy",
+        text2: "Customization",
+        line: "Ireava comes with custom select options in which a user can configure system functionality to fit ERP for its use. Ireava also does on-demand development for ease of work and to make it completely usable."
+    }, {
+        icon: "wide_rang_features",
+        iconalt: "school software features",
+        text1: "Range of",
+        text2: "Features",
+        line: "Ireava understands how time is precious, so we provides a wide range of useful features to users in school software that works as an automated system that provides easy control and a quick work completion process."
+    }, {
+        icon: "secure_cloud_host",
+        iconalt: "secure cloud host",
+        text1: "Secure Cloud",
+        text2: "Host",
+        line: "Ireava is using the best hosting server, so that users will be able to use Ireava school management software anytime, anywhere without downtime. A user will be able to check or upload data quickly without any hassle."
+    }, {
+        icon: "realtime_access",
+        iconalt: "Real time access",
+        text1: "Real Time",
+        text2: "access",
+        line: "Ireava provides real-time data access and storage in school ERP that helps a user to move data across the enterprise faster and on-demand without loading again and again. Ireava ERP is a completely centralized system."
+    }, {
+        icon: "multiple_language_support",
+        iconalt: "Multiple languages support",
+        text1: "Languages",
+        text2: "Supported",
+        line: "Ireava provides multiple languages support in school management software so that users can access data in their own language and focus on work regardless of any language barrier."
+    }, {
+        icon: "money_back",
+        iconalt: "15 days money back",
+        text1: "15 Days Money",
+        text2: "Back",
+        line: "Use Ireava school management software and if you find Ireava software is not suitable for you within 15 days then get your money back. Some terms apply to money-back policies."
+    }, {
+        icon: "custmize_new_module",
+        iconalt: "school software customized modules",
+        text1: "Customize",
+        text2: "Modules",
+        line: "Call us and tell us customized requirements for any module in school management software. Ireava provides the best customized solution for users' requirements."
+    }, {
+        icon: "free_update_software",
+        iconalt: "school software updates",
+        text1: "Regular",
+        text2: "Updates",
+        line: "Ireava provides updates of school management software with new modules regularly. A user will get all updated system time to time that makes an ERP fully updated as per need."
+    }, {
+        icon: "al_plugin",
+        iconalt: "ireava plugins",
+        text1: "Ireava",
+        text2: "plugins",
+        line: "Ireava integrates and releases new plugins in school ERP regularly to make it better, quick, and easy to use. It helps to reduce manual work and increase efficiency in work."
+    }],
+    homelist5 = [{
+        icon: "fas fa-cogs",
+        title: "Simple and easy to use for everyone",
+        text: "Designed to engage every school member. No expertise is required to accomplish a task. It's easy, low-priced, fast and trouble-free school management software."
+    }, {
+        icon: "fas fa-rocket",
+        title: "Powerful elite modules for everyone",
+        text: "Ireava provides modules that will need to run schools, colleges, and institutions smoothly. Elite features in school management software that saves time and lots of manual effort."
+    }, {
+        icon: "fas fa-balance-scale",
+        title: "Quick and automate ERP for everyone",
+        text: "Ireava provides automated modules in school management software that work itself. All modules are designed to work quickly in every perspective."
+    }];
+
+var basicerp2 = [
+    {
+        link: 'javascript:void(0)',
+        text: 'Staff Android/iOS App'
+    },
+    {
+        link: 'javascript:void(0)',
+        text: 'Parent/Student Android/iOS App'
+    },
+    {
+    link: '/multi-branch',
+    text: 'Multi Branch Management'
+}, {
+    link: '/receptiondesk',
+    text: 'Reception Desk Management'
+}, {
+    link: '/student-management',
+    text: 'Student Management'
+}, {
+    link: '/fees-management',
+    text: 'Fees Management'
+}, {
+    link: '/examination-management',
+    text: 'Exam Management'
+}, {
+    link: '/accounting-management',
+    text: '15+ Finance Reports'
+}, {
+    link: '/attendance-management',
+    text: 'Attendance Management'
+}, {
+    link: '/human-resource',
+    text: 'Staff Management'
+}, {
+    link: '/academics-management',
+    text: 'Class Management'
+}, {
+    link: '/academics-management',
+    text: 'Timetable Management'
+}, {
+    link: '/communication-management',
+    text: 'Email/SMS/WhatsApp'
+}, {
+    link: '/communication-management',
+    text: 'Circular Management'
+}, {
+    link: '/communication-management',
+    text: 'Push Notifications'
+}, {
+    link: '/receptiondesk',
+    text: 'Inquiry Management'
+}, {
+    link: '/receptiondesk',
+    text: 'Complaints Helpdesk'
+}, {
+    link: '/smart-dashboard',
+    text: 'Smart Dashboard'
+}, {
+    link: '/human-resource',
+    text: 'Staff Rating Integration'
+}, {
+    link: '/hostel-management',
+    text: 'Hostel Management'
+}, {
+    link: '/inventory-management',
+    text: 'Inventory Management'
+}, {
+    link: '/transport-management',
+    text: 'Transport Management'
+}, {
+    link: '/alumni',
+    text: 'Alumni Management'
+}, {
+    link: '/reports-and-certificates',
+    text: '40+ Data Reports'
+}, {
+    link: '/customized-setting',
+    text: 'Roles & Permissions'
+}, {
+    link: 'javacript:void(0)',
+    text: 'Encrypted Data'
+}, {
+    link: '/customized-setting',
+    text: 'Customized Setting'
+}, {
+    link: '/receptiondesk',
+    text: 'Visitors Management'
+}, {
+    link: '/documents-and-activities-manager',
+    text: 'Docs & Activities'
+}, {
+    link: '/form-builder',
+    text: 'Design Admission Form'
+}, {
+    link: '/additional-features',
+    text: 'School Holiday Calendar'
+}, {
+    link: '/leave-management',
+    text: 'Leave Management'
+}, {
+    link: '/receptiondesk',
+    text: 'Gate Pass Module'
+}, {
+    link: '/fees-management',
+    text: 'Generate Due Fees Slips'
+}, {
+    link: '/academics-management',
+    text: 'Display School Scholars'
+}, {
+    link: '/task-management',
+    text: 'Schedule Reminders'
+}, {
+    link: '/transport-management',
+    text: 'Bus Tracking'
+}, {
+    link: '/attendance-management',
+    text: 'RFID/Barcode Attendance'
+}, {
+    link: '/custom-certificate',
+    text: 'Certificate Design'
+}, {
+    link: '/custom-certificate',
+    text: 'ID Card Generator'
+}, {
+    link: '/website-builder',
+    text: 'Website Management'
+}, {
+    link: '/online-admission',
+    text: 'Online Admission Module'
+}, {
+    link: '/receptiondesk',
+    text: 'Job form'
+}, {
+    link: '/chat',
+    text: 'Real Time Chatting'
+}, {
+    link: '/library-management',
+    text: 'E-Library Management'
+}, 
+{
+    link: '/lesson-plan',
+    text: 'Lesson Plan Module'
+}, {
+    link: '/content-system',
+    text: 'Study Center'
+}, 
+
+{
+    link: '/online-exam',
+    text: 'Online Exam Management'
+},
+{
+    link: 'javascript:void(0)',
+    text: 'Interactive WhiteBoard'
+},
+{
+    link: '/custom-payment-forms',
+    text: 'Dynamic Form Builder'
+}, {
+    link: '/student-rewards',
+    text: 'Student Reward Points'
+}];
